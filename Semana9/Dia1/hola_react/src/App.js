d6fcec4ebcfd843e0e9276f0adeb7b00d7efacc8
jach88 <33399125+jach88@users.noreply.github.com>
2021-07-26 21:13:19 -0500

    update

---
+++ Semana9/Dia1/hola_react/src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hola mundooooooo
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+export default function App() {
+  return (
+    <div>
+      holas <br />
+      aqui se colocara todo el codigo de React
+    </div>
+
+  )
+}
