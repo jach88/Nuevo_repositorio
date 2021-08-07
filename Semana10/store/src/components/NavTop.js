7059d1b5bae0bd4bc093914ce8b316f6a69b1a66
jach88 <33399125+jach88@users.noreply.github.com>
2021-08-06 22:50:37 -0500

    update

--- Semana10/store/src/components/NavTop.js
+++ Semana10/store/src/components/NavTop.js
@@ -1,7 +1,19 @@
+import { useContext } from "react";
+import { CarritoContext } from "../context/carritoContext";
+
 import { Navbar, Container, Nav } from "react-bootstrap";
 import {Link} from "react-router-dom"
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Badge from '@material-ui/core/Badge';
 
 export default function NavTop() {
+
+	const { carrito } = useContext(CarritoContext)
+
+	const totalCarrito = carrito.reduce((total, item) => {
+		return total + item.cantidad
+	}, 0)
+
 	return (
 		<Navbar bg="light" expand="lg">
 			<Container>
@@ -14,12 +26,23 @@ export default function NavTop() {
 								Inicio
 							</Link>
 						</Nav.Link>
-						<Nav.Link>
+						{/* <Nav.Link>
 							<Link to="/carrito">
 								Carrito
+								<Badge badgeContent={totalCarrito} color="primary">
+									<ShoppingCartIcon />
+								</Badge>
+							</Link>
+						</Nav.Link> */}
+					</Nav>
+					<Nav.Link>
+							<Link to="/carrito">
+								Carrito
+								<Badge badgeContent={totalCarrito} color="primary">
+									<ShoppingCartIcon />
+								</Badge>
 							</Link>
 						</Nav.Link>
-					</Nav>
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
--- Semana10/store/src/views/CarritoView.js
+++ Semana10/store/src/views/CarritoView.js
@@ -19,6 +19,7 @@ export default function CarritoView() {
                     <tr>
                         <th>Nombre</th>
                         <th>Cantidad</th>
+                        <th>Descuento</th>
                         <th>Precio Unitario</th>
                         <th>Precio Total</th>
                     </tr>
@@ -27,9 +28,12 @@ export default function CarritoView() {
                     {carrito.map((prod, i) => (
                         <tr key={i}>
                             <td>{prod.prod_nombre}</td>
-                            <td>{prod.prod_cantidad}</td>
-                            <td>{prod.prod_precio}</td>
-                            <td></td>
+                            <td>{prod.cantidad}</td>
+                            <td>{prod.prod_oferta ? "10%" : "Sin Oferta"}</td>
+                            <td>S/ {prod.prod_precio}</td>
+                            <td>S/ {prod.prod_oferta ? 
+                            prod.cantidad * prod.prod_precio * 0.9 : 
+                            prod.cantidad * prod.prod_precio}</td>
                         </tr>
                     ))}
                 </tbody>
